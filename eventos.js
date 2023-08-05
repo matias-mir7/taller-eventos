2b626c264d30103401e7c17d93a18a9916be4f2e
matias-mir7 <gcmmir@gmail.com>
2023-08-05 18:07:25 -0300

    Sitio web creado

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const click = document.querySelector("div");
+    click.addEventListener("click", function () {
+        alert("Hola! Soy el div");
+      });
+    });
